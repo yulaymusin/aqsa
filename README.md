0ce0986220b4f9d7cacb45f57bf7c1a03b5026fa
Yulay Musin <yulaymusin@gmail.com>
2018-10-12 01:20:25 +0500

    Launched on 'theaqsa.com'

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Aqsa
 Aqsa is Personal Finance Software.
 Aqsa is simple, but free software which have some powerful functionality even compare some commercial software.
+## Live of Aqsa
+https://theaqsa.com
--- aqsa_apps/wsgi.py
+++ aqsa_apps/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for aqsa project.
+WSGI config for aqsa_apps project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aqsa.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aqsa_apps.settings")
 
 application = get_wsgi_application()
